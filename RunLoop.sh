953c6fca6727ace7678dc0a3c4076a23ddef05f3
Sophie <sophielien09@gmail.com>
2026-03-10 12:31:01 +0800

    init

--- RunLoop.sh
+++ RunLoop.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 TOTAL_RUNS=2                         # << set to 1000 for one thousand runs
-RUN_TIME_PER_GAME_SEC="${RUN_TIME_PER_GAME_SEC:-90}"  # << set default seconds per run it needs to be 500
+RUN_TIME_PER_GAME_SEC="${RUN_TIME_PER_GAME_SEC:-500}"  # << set default seconds per run it needs to be 500
 
 mkdir -p logs bin
 
@@ -16,7 +16,8 @@ for ((i=1; i<=TOTAL_RUNS; i++)); do
   javac -cp "lib/*:bin" -d bin @sources.list
 
   echo "[INFO] starting game (will auto-stop after ${RUN_TIME_PER_GAME_SEC}s)..."
-  java -cp "lib/*:bin" rts.MicroRTS >"$LOGFILE" 2>&1 &
+  java -cp "lib/*:bin" gui.frontend.FrontEnd >"$LOGFILE" 2>&1 &
+  # java -cp "lib/*:bin" rts.MicroRTS >"$LOGFILE" 2>&1 &
   game_pid=$!
 
   sleep "$RUN_TIME_PER_GAME_SEC"
